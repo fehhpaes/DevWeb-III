24bde0ff4b98cda2f9bbd2f28b6ffec405eb9a31
Felipe Paes da Silva <felipe.silva638@fatec.sp.gov.br>
2025-08-27 14:38:38 -0300

    Criação post de alunos e cursos

--- ExercicioAlunos/app.js
+++ ExercicioAlunos/app.js
@@ -2,9 +2,30 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+
+app.use(express.json())
+
+let alunos = [];
+let cursos = [];
+
+//first endpoint
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.send(alunos);
+  res.send(cursos);
 })
+
+app.post('/', (req, res) => {
+  const {ra, nome, turma, cursos} = req.body;
+
+  alunos.push({ra:ra, nome:nome, turma:turma, cursos:cursos});
+ 
+ res.send(JSON.stringify(alunos));
+});
+
+
+
+
+
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
