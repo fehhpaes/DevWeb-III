d7f3844e202a49f45702b2d94522cd448f65157f
Felipe Paes da Silva <felipe.silva638@fatec.sp.gov.br>
2025-08-29 11:27:13 -0300

    Alteração comando post, separação para aluno e curso

--- ExercicioAlunos/app.js
+++ ExercicioAlunos/app.js
@@ -5,30 +5,54 @@ const port = 3000
 
 app.use(express.json())
 
-let alunos = [];
-let cursos = [];
+let alunos = [{
+  "ra": "1",
+  "nome": "Aurora",
+  "turma": "DSM",
+  cursos: ["Matematica","Desenvolvimento Web III", "Técnicas de Programação"]
+},
+{
+  "ra": "2",
+  "nome": "Bruno",
+  "turma": "ADS",
+  cursos: []
+}];
 
-//first endpoint
-app.get('/', (req, res) => {
-  res.send(alunos);
-  res.send(cursos);
-})
 
-app.post('/', (req, res) => {
-  const {ra, nome, turma, cursos} = req.body;
 
-  alunos.push({ra:ra, nome:nome, turma:turma, cursos:cursos});
- 
- res.send(JSON.stringify(alunos));
+app.post('/alunos', (req, res) => {
+  const {ra, nome, turma} = req.body
+  const novoAluno = {ra, nome, turma, cursos:[]};
+  //const novoAluno = {ra, nome, turma, cursos:[curso1,curso2,curso3,curso4,curso5,curso6,curso7]};
+  alunos.push(novoAluno);
+  
+  res.send(JSON.stringify(novoAluno));
 });
 
-app.put('/', (req, res) => {
-  const index = alunos.findIndex(x => x.ra == req.body.ra );
-  alunos[index] = {ra: req.query.ra, nome: req.body.nome, turma: req.body.turma, cursos: req.body.cursos};
-  res.send(JSON.stringify(alunos));
+app.post('/alunos/:ra/cursos', (req, res) => {
+  const {ra} = req.params;
+  const cursos = {cursos:[]} = req.body;
+  const aluno = alunos.find(a => a.ra === ra);
+ 
+  aluno.cursos.push(cursos);
+
+  res.send(JSON.stringify(aluno.cursos));
 })
 
 
+app.put('/', (req, res) => {
+  // const index = alunos.findIndex(x => x.ra == req.body.ra );
+  // alunos[index] = {ra: req.query.ra, nome: req.body.nome, turma: req.body.turma, cursos: req.body.cursos};
+  // res.send(JSON.stringify(alunos));
+})
+
+app.delete('/', (req, res) => {
+  // const index = alunos.findIndex(x => x.ra == req.query.ra );
+  // //const indexCurso = cursos.findIndex(y => y.id == req.query.id );
+  // alunos.splice(index, 1);
+  // //cursos.splice(indexCurso, 1)
+  // res.send(JSON.stringify(alunos));
+})
 
 
 app.listen(port, () => {
